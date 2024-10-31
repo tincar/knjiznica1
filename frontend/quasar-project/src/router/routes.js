63c5a7f2771c9bb91817c3cdccb6a424fac38f0b
tincar <tin.car543@gmail.com>
2024-10-31 13:05:34 +0100

    Kreiranje Quasar projekta

---
+++ frontend/quasar-project/src/router/routes.js
@@ -0,0 +1,24 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/popisKnjiga', component: () => import('pages/PopisKnjigaPage.vue') },
+      { path: '/pretrazivanje', component: () => import('pages/TraziKnjiguPage.vue') },
+      { path: '/o_nama', component: () => import('pages/ONamaPage.vue') },
+      { path: '/lokacija', component: () => import('pages/LokacijaPage.vue') },
+      { path: '/login', component: () => import('pages/LoginPage.vue') },
+      { path: '/registracija', component: () => import('pages/RegistracijaPage.vue') },
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
